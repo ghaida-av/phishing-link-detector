af0e530596b3a951c2f6c074709f5487e2895c90
ghaida-av <ghaidaasan97@gmail.com>
2025-08-27 09:34:09 +0400

    Update data_cleaning.py

--- phish_detector/phish_detector/phish_detector/data_cleaning.py
+++ phish_detector/phish_detector/phish_detector/data_cleaning.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Basic data cleaning:
+    - Drop missing URLs
+    - Remove duplicates
+    """
+    df = df.dropna(subset=["url"])
+    df = df.drop_duplicates(subset=["url"])
+    return df
 
