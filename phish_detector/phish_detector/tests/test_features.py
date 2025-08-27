38a769d15ca9ea85d5bf4042b9c1872185746096
ghaida-av <ghaidaasan97@gmail.com>
2025-08-27 09:35:26 +0400

    Update test_features.py

--- phish_detector/phish_detector/tests/test_features.py
+++ phish_detector/phish_detector/tests/test_features.py
@@ -1,1 +1,8 @@
+from phish_detector.features import get_feature_pipeline
+
+def test_pipeline_runs():
+    pipeline = get_feature_pipeline()
+    sample = ["http://example.com", "http://phishy.com/login"]
+    X = pipeline.fit_transform(sample)
+    assert X.shape[0] == 2
 
